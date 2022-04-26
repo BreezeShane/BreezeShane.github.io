09634bd1e8c4994f4847397538d6298bcb6f9e23
BreezeShane <346869441@qq.com>
2022-04-26 10:42:01 +0800

    Removed dependencies and adjusted the structure of docs.

--- node_modules/.temp/internal/global-ui.js
+++
@@ -1,6 +0,0 @@
-export default [
-  "BackToTop",
-  "SWUpdatePopup",
-  "PWAInstall",
-  "PhotoSwipeUI"
-]
--- node_modules/workbox-expiration/_version.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// @ts-ignore
-try {
-    self['workbox:expiration:6.1.5'] && _();
-}
-catch (e) { }
